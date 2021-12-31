a2a11664463e37b012aba28401d9d16f3a6ae946
Olivér <oliver.mrakovics@gmail.com>
2021-12-31 08:48:15 +0100

    added UpdateBook function.

--- cmd/crud/main.go
+++ cmd/crud/main.go
@@ -28,6 +28,7 @@ func main() {
 	app.Get("/api/book/:id", handlers.GetBook)
 	app.Post("/api/addbook", handlers.AddBook)
 	app.Delete("/api/deletebook/:id", handlers.DeleteBook)
+	app.Put("/api/updatebook/:id", handlers.UpdateBook)
 
 	app.Listen(fmt.Sprintf(":%v", port))
 }
---
+++ pkg/handlers/updateBook.go
@@ -0,0 +1,30 @@
+package handlers
+
+import (
+	"fmt"
+
+	"github.com/0l1v3rr/go-crud/pkg/data"
+	"github.com/gofiber/fiber/v2"
+)
+
+func UpdateBook(c *fiber.Ctx) error {
+	book, err := data.GetBookById(c.Params("id"))
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).SendString("Not Found")
+	}
+
+	for index, item := range data.Books {
+		if item.ID == book.ID {
+			data.Books = append(data.Books[:index], data.Books[index+1:]...)
+			book := new(data.Book)
+			if err := c.BodyParser(book); err != nil {
+				fmt.Println(err)
+				return c.SendStatus(200)
+			}
+			data.Books = append(data.Books, *book)
+			break
+		}
+	}
+
+	return c.JSON(data.Books)
+}
